5437c947c4179244c6e232158ac61202f9416a4a
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-20 18:19:35 +0100

    Made the blog latest post page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import Blog_Post
+def all_posts(request):
+    blogs = Blog_Post.objects
+    return render(request, 'blog/home.html', {'blogs':blogs})
 
 # Create your views here.
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',jobs.views.home, name='home'),	
+    path('',jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),	
    
     	
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # adding static files where to look for info and url path
