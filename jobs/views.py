d59ac409552c8f964b1981684a94684ac54cd596
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-20 11:19:04 +0100

    Home Page edited to look pretty

--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import datetime
 
 
 def home(request):
+    #now = datetime.datetime.now()
+    #current_year = now.year
     return render(request, 'jobs/home.html')
 
 # Create your views here.
