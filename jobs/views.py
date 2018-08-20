1b43d8f2227a7fecbc92d80d3f0f3250592f22dc
Gareth Breeze <gareth.breeze1993@gmail.com>
2018-08-20 16:41:26 +0100

    Added jobs model to the homepage

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 import datetime
+from .models import Job 
 
 
 def home(request):
-    #now = datetime.datetime.now()
-    #current_year = now.year
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects # this will get all our objects from the job database.
+    return render(request, 'jobs/home.html', {'jobs':jobs})
 
 # Create your views here.
